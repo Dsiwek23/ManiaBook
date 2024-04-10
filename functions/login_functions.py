74216524c0aaea3729da159b0bd7e8e6b9bb2d63
Dominik Siwek <136427725+Dsiwek23@users.noreply.github.com>
2024-04-10 17:00:22 +0200

    login_functions.py

--- functions/login_functions.py
+++ functions/login_functions.py
@@ -2,7 +2,7 @@ from flask import request, redirect, url_for, render_template, session
 from flask_bcrypt import Bcrypt
 from models import (
     User,
-)  # Zakładam, że model User znajduje się w pliku models.py lub w tym samym pliku co funkcje
+)
 
 bcrypt = Bcrypt()
 
@@ -12,14 +12,12 @@ def login_user(app):
         username = request.form["username"]
         password = request.form["password"]
 
-        # Sprawdź, czy to jest administrator
         if username == "admin" and bcrypt.check_password_hash(
             app.config["ADMIN_PASSWORD_HASH"], password
         ):
             session["admin_logged_in"] = True
             return redirect(url_for("admin_panel"))
 
-        # Sprawdź, czy to jest użytkownik
         user = User.query.filter_by(username=username).first()
 
         if user and bcrypt.check_password_hash(user.password, password):
